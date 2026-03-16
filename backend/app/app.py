325378b511bce27ddbaa1c6347980c38352bcad6
minhnhuthihi <klemin3005@gmail.com>
2026-03-16 18:17:05 +0700

    add env

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,16 +1,15 @@
-from flask import Flask, request, jsonify
-from flask import render_template
+import os
+import base64
+from io import BytesIO
 from flask_cors import CORS
-from services.predict_service import predict_image
-from services.gradcam_service import run_gradcam
-from schemas.response_schema import PredictionData, ApiResponse
-from utils.dicom_util import dicom_to_preview
-
+from flask import render_template
+from flask import Flask, request, jsonify
 from routes.preview_routes import preview_bp
 from routes.predict_routes import predict_bp
-# Các import khác giữ nguyên...
-from io import BytesIO
-import base64
+from utils.dicom_util import dicom_to_preview
+from services.gradcam_service import run_gradcam
+from services.predict_service import predict_image
+from schemas.response_schema import PredictionData, ApiResponse
 
 
 app = Flask(
@@ -29,6 +28,7 @@ app.register_blueprint(preview_bp)
 app.register_blueprint(predict_bp)
 
 if __name__ == "__main__":
-    print("FLASK STARTED SUCCESSFULLY")
-    app.run(host="0.0.0.0", port=5001)
-  
+    port = int(os.environ.get("PORT", 8080))  # mặc định 5001 nếu không có env
+    debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    print(f"✅ FLASK STARTED — http://localhost:{port}")
+    app.run(host="0.0.0.0", port=port, debug=debug)
