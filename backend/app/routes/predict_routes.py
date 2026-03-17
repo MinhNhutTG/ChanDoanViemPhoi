ae0f1ac2681f6bed89f35e49259245a18de1305e
minhnhuthihi <klemin3005@gmail.com>
2026-03-17 19:06:09 +0700

    fix model.pth

--- backend/app/routes/predict_routes.py
+++ backend/app/routes/predict_routes.py
@@ -3,8 +3,24 @@ from services.predict_service import predict_image
 from services.gradcam_service import run_gradcam
 from schemas.response_schema import PredictionData, ApiResponse
 from io import BytesIO
+import os
+import gdown
+import torch
+
+
 
 predict_bp = Blueprint("predict", __name__, url_prefix="/api")
+
+MODEL_PATH = "models/best_resnet50_rsna.pth"
+
+# Download model nếu chưa tồn tại
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("models", exist_ok=True)
+    gdown.download(
+        "https://drive.google.com/file/d/1nmYQxFRqEZX5SUOo2XliEpII3RbwMb4T/view?usp=sharing",
+        MODEL_PATH,
+        quiet=False
+    )
 
 
 @predict_bp.route("/predict", methods=["POST"])
@@ -25,7 +41,7 @@ def predict_api():
     # GradCAM
     pred_class, heatmap_base64 = run_gradcam(
         file_for_gradcam,
-        checkpoint_path="models/best_resnet50_rsna.pth",
+        checkpoint_path=MODEL_PATH,
         device="cpu"
     )
 
